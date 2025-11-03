b146963f940890734cb7db4489f304eddeb31216
Yesenia Tracy Nitha <yeseniatracynitha@gmail.com>
2025-11-04 02:25:08 +0800

    Inisialisasi struktur backend: model, controller, route dasar

--- sistem-absensi/sistem-absensi/routes/web.php
+++ sistem-absensi/sistem-absensi/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\AbsensiController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/dosen', [DosenController::class, 'index']);
+Route::get('/absensi', [AbsensiController::class, 'index']);
